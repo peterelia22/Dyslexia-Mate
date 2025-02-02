81505771e9de84ac04f8903ebdd0aa0d34945ad6
peterelia22 <peterelia914@gmail.com>
2025-02-02 13:56:56 +0200

    resolve issue with GameLauncher deep link handling

--- lib/helpers/game_launcher.dart
+++ lib/helpers/game_launcher.dart
@@ -2,18 +2,37 @@ import 'package:url_launcher/url_launcher.dart';
 
 class GameLauncher {
   static Future<void> launchGame() async {
-    final Uri deepLink = Uri.parse("theforgottenletters://start");
+    final Uri intentUri = Uri.parse("theforgottenletters://start");
 
-    if (await canLaunchUrl(deepLink)) {
-      await launchUrl(deepLink);
-      return;
+    try {
+      print("Attempting to launch deep link: $intentUri");
+      if (await canLaunchUrl(intentUri)) {
+        print("Deep link is supported. Launching...");
+        await launchUrl(intentUri);
+      } else {
+        print("Deep link is not supported. Falling back to download link...");
+        await launchGameDownload();
+      }
+    } catch (e) {
+      print("Deep link failed: $e");
+      print("Falling back to download link...");
+      await launchGameDownload();
     }
+  }
 
-    final Uri playStoreUri =
-        Uri.parse("https://aliwafa.itch.io/the-forgotten-letters");
+  static Future<void> launchGameDownload() async {
+    final String gameUrl = "https://aliwafa.itch.io/the-forgotten-letters";
+    final Uri uri = Uri.parse(gameUrl);
 
-    if (await canLaunchUrl(playStoreUri)) {
-      await launchUrl(playStoreUri, mode: LaunchMode.externalApplication);
+    try {
+      print("Attempting to launch download link: $gameUrl");
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        print("Could not launch $gameUrl");
+      }
+    } catch (e) {
+      print("Error launching download link: $e");
     }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import package_info_plus
 import shared_preferences_foundation
+import uni_links_desktop
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UniLinksDesktopPlugin.register(with: registry.registrar(forPlugin: "UniLinksDesktopPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
